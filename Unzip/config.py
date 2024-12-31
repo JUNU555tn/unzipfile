8ffed0a481e393502eb9ba97cd19dcf71fbf84ef
JUNU555tn <105146928+JUNU555tn@users.noreply.github.com>
2024-12-31 15:39:11 +0600

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -1,9 +1,16 @@
 import os
+import re
+
+# Define the ID pattern for channel IDs
+id_pattern = re.compile(r'^.\d+$')
+
+# Get the AUTH_CHANNEL environment variable and process it
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in os.environ.get('AUTH_CHANNEL', '').split()]  # Space-separated channel IDs
 
 class config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     API_ID = int(os.environ.get("API_ID", 0))
     API_HASH = os.environ.get("API_HASH", "")
-    FORCE_SUB_CHANNELS = os.environ.get("FORCE_SUB_CHANNELS", "").split("-1002135593873,-1001764441595")  # Add multiple channel IDs separated by commas
-
-
+    
+    # You can access AUTH_CHANNEL directly from here if needed
+    AUTH_CHANNEL = AUTH_CHANNEL
