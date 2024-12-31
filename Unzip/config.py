5ae44a0ba04318e8128272ead10ecd5ea13c8f90
JUNU555tn <105146928+JUNU555tn@users.noreply.github.com>
2024-12-31 14:22:32 +0600

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config(object):
-     
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
+    API_ID = int(os.environ.get("API_ID", 0))
     API_HASH = os.environ.get("API_HASH", "")
-    
-    
+    FORCE_SUB_CHANNELS = os.environ.get("FORCE_SUB_CHANNELS", "").split("-1002135593873,-1001764441595")  # Add multiple channel IDs separated by commas
+
+
